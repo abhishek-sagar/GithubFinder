fc4b86ca9c6a4fe0d2722f16b320833ae12261f9
abhishek-sagar <sagar.abhishek1996@gmail.com>
2018-07-24 17:38:44 +0530

    Create App V1

---
+++ app.js
@@ -0,0 +1,34 @@
+//init github
+const github = new Github;
+
+const ui = new UI;
+
+//Search input
+const searchUser = document.getElementById("searchUser");
+
+//Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    //Get input text
+    const userText = e.target.value;
+
+    if(userText !== '') {
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === "Not Found") {
+                //Show alert
+                
+                ui.showAlert('User not found', 'alert alert-danger');
+            }
+            else {
+                //Show profile
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+        })
+    } 
+    else {
+        //clear profile
+        console.log("clear");
+        
+    }
+});
